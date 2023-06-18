ff238accae7b88e0fa724b3446b13d2d7b854a51
Faethryn <laurens.muylle@student.howest.be>
2023-06-18 16:06:26 +0200

    Removed the hello isp text

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -71,9 +71,7 @@ void JuceReverbAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello ISP", getLocalBounds(), juce::Justification::centred, 1);
+   
 }
 
 void JuceReverbAudioProcessorEditor::resized()
